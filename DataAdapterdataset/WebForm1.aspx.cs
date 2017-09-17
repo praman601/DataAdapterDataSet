db693f82fefd3805fe97d36704a33a49f39bcead
pradeep budhathoki <pradeep.budhathoki1234@gmail.com>
2017-09-17 11:28:06 +0545

    another commit

---
+++ DataAdapterdataset/WebForm1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DataAdapterdataset
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            using(SqlConnection con = new SqlConnection("Data Source=PRADEEP\\SQLEXPRESS;Initial Catalog=product;Integrated Security=true"))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("sptblProductInventoryById  ", con);
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                da.SelectCommand.Parameters.AddWithValue("@Id", TextBox1.Text);
+
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+            }
+
+
+        }
+    }
+}
